aa9b06b7e1de658efdea5bce10a332645515a9f6
Daniel Turlinski <daniel.turlinski@gmail.com>
2017-11-25 20:54:39 +0100

    Fix missing curly bracket

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,10 @@ var a = 12,
     value = (a * a) - (2 * a * b) + (b * b);
 console.log(value);
 
-if (value > 0 && value != 0 ) {
+if (value > 0) {
 	console.log('Wynik dodatni');
-} else if (value < 0 && value != 0 ) {
+} else if (value < 0 ) {
 	console.log('Wynik ujemny');
 } else {
 	console.log('Wynik równy 0');
+}
