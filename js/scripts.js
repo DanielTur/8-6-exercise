99eac728a5b86d5bdb189eaa7ea1faa2a5bfbe5a
Daniel Turlinski <daniel.turlinski@gmail.com>
2017-11-25 02:42:27 +0100

    Add simple js script

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// scripts.js
+
+var a = 12,
+    b = -23,
+    value = (a * a) - (2 * a * b) + (b * b);
+console.log(value);
+
+if (value > 0 && value != 0 ) {
+	console.log('Wynik dodatni');
+} else if (value < 0 && value != 0 ) {
+	console.log('Wynik dodatni');
+} else {
+	console.log('Wynik równy 0');
+}
